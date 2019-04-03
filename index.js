5d5bda3c5ae001079f9e2ec66f5b533557e307ea
Martin Ramos <marprg@gmail.com>
2019-04-03 12:25:52 -0300

    Class methods implement

--- index.js
+++ index.js
@@ -3,15 +3,28 @@
 
 //Golondrinas: Pueden comer y volar. Cuando comen ganan 2 de energia por cada gramo que consuman y cuando vuelan, pierden 3 de energía por cada km.
 
-export function crearGolondrina(energia) {
-    return {
-        energia: 30,
-        volar: function(km) {
-            this.energia -= km * 3
-        },
-        comer: function(gramos) {
-            this.energia += gramos * 2
-        }
+// export function crearGolondrina(energia) {
+//     return {
+//         energia: 30,
+//         volar: function(km) {
+//             this.energia -= km * 3
+//         },
+//         comer: function(gramos) {
+//             this.energia += gramos * 2
+//         }
+//     }
+// }
+
+export class Golondrina {
+    constructor(energia = 30) {
+        this.energia = energia
+    }
+
+    volar(km) {
+        this.energia -= km * 3
+    }
+    comer(gramos) {
+        this.energia += gramos * 2
     }
 }
 
--- index.spec.js
+++ index.spec.js
@@ -1,19 +1,21 @@
-import { pepita, crearGolondrina } from "./index";
+import { Golondrina } from "./index";
 
 
 describe("pepita básica", () => {
     it("debería volar y consumir energía", () => {
-        const pepita = crearGolondrina()
+        // const pepita = crearGolondrina()
+        const pepita = new Golondrina()
         pepita.volar(2)
         expect(pepita.energia).toEqual(24)
 
     })
 
     it("debería comer y sumar energía", () => {
-        const pepita = crearGolondrina()
+        // const pepita = crearGolondrina()
+        const pepita = new Golondrina()
         pepita.comer(3)
         expect(pepita.energia).toEqual(36)
-        
+
     })
 
 })
